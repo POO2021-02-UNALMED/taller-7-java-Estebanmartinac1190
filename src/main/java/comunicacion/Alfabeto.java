ed60c7c304621fb62f30ad3ed370480ed3ed0c8b
Estebanmartinac1190 <92158255+Estebanmartinac1190@users.noreply.github.com>
2021-12-07 12:35:45 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -1,45 +1,39 @@
 package comunicacion;
+public class Alfabeto extends  Pictograma{
+    private String[] letras;
+    private String interpretacion;
+    public Alfabeto(String origen, String[] letras, String interpretacion) {
+        super(origen);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
 
-public class Alfabeto extends Pictograma {
-	//ATRIBUTOS
-	private String[] letras;
-	private String interpretacion;
-	
-	//CONSTRUCTOR 
-	public Alfabeto(String origen, String[] letras, String interpretacion) {
-		super(origen);
-		this.letras = letras;
-		this.interpretacion = interpretacion;
-	}
-	
-	//SET AND GET 
-	
-	public String getInterpretacion() {
-		return interpretacion;
-	}
-	
-	public void setInterpretacion(String interpretacion) {
-		this.interpretacion = interpretacion;
-	}
-	
-	public String[] getLetras() {
-		return letras;
-	}
-	
-	public void setLetras(String[] letras) {
-		this.letras = letras;
-	}
-	//METODOS
-	public int cantidadLetras() {
-		return letras.length;
-	}
-	
-	public String toString() {
-		String espacio= "";
-		for (String lina: letras) {
-			espacio +=lina+", ";
-		}
-		return espacio.strip().substring(0, espacio.length()-2);
-	}
+    public String[] getLetras() {
+        return letras;
+    }
 
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+
+    public String interpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+
+    public String toString(){
+        String lima = "";
+
+        for(String lupus : letras){
+            lima += lupus+", ";
+        }
+        return lima.strip().substring(0, lima.length()-2);
+    }
+
+    public int cantidadLetras() {
+        return letras.length;
+    }
 }

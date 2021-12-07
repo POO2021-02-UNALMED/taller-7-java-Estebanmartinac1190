c942da53f710e7eef9b7103917f2061f31d7e9c0
Estebanmartinac1190 <92158255+Estebanmartinac1190@users.noreply.github.com>
2021-12-07 12:40:08 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -1,39 +1,28 @@
 package comunicacion;
-
 public class Fabula extends Escrito {
-	private String ensenanza;
-	private String interpretacion;
-	//CONSTRUCTOR
-	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+    private String ensenanza;
+    private String interpretacion;
+    public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
         super(origen, titulo, autor, paginas);
         this.ensenanza = ensenanza;
         this.interpretacion = interpretacion;
     }
-	
-	//SET AND GET 
     public String getEnsenanza() {
         return ensenanza;
     }
-
     public void setEnsenanza(String ensenanza) {
         this.ensenanza = ensenanza;
     }
-
     public String interpretacion() {
-        return interpretacion;
+        return interpretiacion;
     }
-
     public void setInterpretacion(String interpretacion) {
         this.interpretacion = interpretacion;
     }
-    
-    //METODOS 
-    
-    public int palabrasTotales(int loca) {
-    	return this.getPaginas()*loca;
-    }
-    
     public String toString() {
-    	return this.getOrigen() + "\n" + this.getTitulo() + "\n" + this.getAutor() + "\n" + this.getPaginas() + "\n" + this.ensenanza;
+        return this.getOrigen() + "\n" + this.getTitulo() + "\n" + this.getAutor() + "\n" + this.getPaginas() + "\n" + this.ensenanza;
+    }
+    public int palabrasTotales(int yu) {
+        return this.getPaginas()*yu;
     }
 }
